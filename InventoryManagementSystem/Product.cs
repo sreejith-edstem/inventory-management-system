ea3b1969f30db9cb34d75e829a53c15add92cbf4
sreejith-edstem <sreejith.s@edstem.com>
2024-10-30 10:10:21 +0530

    initialized the class

---
+++ InventoryManagementSystem/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    internal class Product
+    {
+        public string ProductId { get; set; }  // Format: XXX-999
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+        public DateTime LastRestocked { get; set; }
+    }
+}
